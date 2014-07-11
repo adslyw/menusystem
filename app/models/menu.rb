e6914a74e0b51c10c82646b29e085a399006e571
adslyw <adslyw@gmail.com>
2014-07-11 11:30:13 +0800

    menu modle bug fixed

--- app/models/menu.rb
+++ app/models/menu.rb
@@ -2,5 +2,5 @@ class Menu < ActiveRecord::Base
   attr_accessible :icon, :title, :url
   validates :title, presence: true
   validates :icon, presence: true
-  has_many :submenus, dependent: [:destroy, :update]
+  has_many :submenus, dependent: :destroy
 end
